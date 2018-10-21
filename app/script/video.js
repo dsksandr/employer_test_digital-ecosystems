560909ab1a5243b652192068acf3b64d6e9243be
ds_ksandr <zhilin_aleksandr@live.ru>
2018-10-21 18:05:48 +0700

    Добавил разметку для section shop

--- app/script/video.js
+++ app/script/video.js
@@ -1,15 +1,14 @@
 let overlay = document.querySelector('.overlay'),
-    video = document.querySelector('#video');
-
-if (overlay.addEventListener) {
-    overlay.addEventListener("click", play, false)
-}
+  video = document.querySelector('#video');
 function play() {
-    if (video.paused) {
-        video.play();
-        overlay.classList.add('play');
-    } else {
-        video.pause();
-        overlay.classList.remove('play');
-    }
+  if (video.paused) {
+    video.play();
+    overlay.classList.add('play');
+  } else {
+    video.pause();
+    overlay.classList.remove('play');
+  }
+}
+if (overlay.addEventListener) {
+  overlay.addEventListener("click", play, false)
 }
