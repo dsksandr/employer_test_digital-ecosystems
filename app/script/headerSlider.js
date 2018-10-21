fe5a9667939217451036396b50f7378ef75a59fc
ds_ksandr <zhilin_aleksandr@live.ru>
2018-10-21 16:30:11 +0700

    Скрипты для работы видео и слайдера

---
+++ app/script/headerSlider.js
@@ -0,0 +1,32 @@
+window.onload = () => {
+    let slides = document.querySelectorAll('#headerSliderUl .slide'),
+        currentSlide = 0,
+        //slideInterval = setInterval(nextSlide,5000),
+        pageNumber = document.querySelector('.pageNumber');
+
+    function nextSlide() {
+        goToSlide(currentSlide + 1);
+    }
+
+    function previousSlide() {
+        goToSlide(currentSlide - 1);
+    }
+
+    function goToSlide(n) {
+        slides[currentSlide].className = 'slide';
+        currentSlide = (n+slides.length)%slides.length;
+        slides[currentSlide].className = 'slide showSlide';
+        pageNumber.innerHTML = `<span>${currentSlide + 1} </span>/<span> ${slides.length}</span>`;
+    }
+
+    let next = document.getElementById('nextSlide'),
+        previous = document.getElementById('previousSlide');
+
+    next.onclick = function() {
+        nextSlide();
+    };
+
+    previous.onclick = function() {
+        previousSlide();
+    };
+};
